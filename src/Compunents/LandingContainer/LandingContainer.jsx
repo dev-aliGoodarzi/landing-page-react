8aaaa68a64e23fa979025eee724544c74aa4744e
Ali <me74242@gmail.com>
2022-08-03 12:23:32 +0430

    start responsive for mobile

--- src/Compunents/LandingContainer/LandingContainer.jsx
+++ src/Compunents/LandingContainer/LandingContainer.jsx
@@ -4,13 +4,13 @@ import { bgTop, billyImg } from "../ImageExporter/ImageExporter";
 
 const LandingContainer = () => {
   return (
-    <div className="w-full flex flex-col align-start justify-start p-20 pt-5 main-container__custom">
+    <div className="2xl:w-full 2xl:flex 2xl:flex-col 2xl:align-start 2xl:justify-start 2xl:p-20 2xl:pt-5 main-container__custom">
       <div className="custom__img">
         <img src={bgTop} alt="" />
       </div>
       <Header />
-      <div className="bottom__container w-full flex flex-row align-cetner justify-between p-10 ">
-        <div className="left w-1/2 flex flex-col align-start justify-start">
+      <div className="bottom__container 2xl:w-full 2xl:flex 2xl:flex-row 2xl:align-cetner 2xl:justify-between 2xl:p-10 ">
+        <div className="left 2xl:w-1/2 flex 2xl:flex-col 2xl:align-start 2xl:justify-start">
           <h1 className={`text-white `}>
             We are The Brilliants In
             <br />
@@ -18,13 +18,15 @@ const LandingContainer = () => {
             <br />
             Marketing
           </h1>
-          <p className={`text-white mt-5`}>
+          <p className={`text-white 2xl:mt-5`}>
             Book your 30-minute free strategy call
           </p>
-          <button className={`h-16 w-64 bg-white mt-10 `}>Book Now</button>
+          <button className={`2xl:h-16 2xl:w-64 bg-white 2xl:mt-10 `}>
+            Book Now
+          </button>
         </div>
-        <div className="right w-1/2 flex flex-row align-start justify-start ">
-          <img src={billyImg} alt="" className={`w-full`} />
+        <div className="right 2xl:w-1/2 flex 2xl:flex-row 2xl:align-start 2xl:justify-start ">
+          <img src={billyImg} alt="" className={`2xl:w-full`} />
         </div>
       </div>
     </div>
